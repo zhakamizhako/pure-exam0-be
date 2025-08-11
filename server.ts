d831f4e34ac109a0aa6da1379da08fea650a65bf
Roland Kim Andre Solon <solon.rolandkimandre@gmail.com>
2025-08-11 17:53:00 +0800

    server.ts

--- server.ts
+++ server.ts
@@ -1,9 +1,6 @@
 import routes from './routes/records'
-import recordInstance from './utils/records'
-const express = require('express')
-
-let recordInstanced = new recordInstance();
-
+import cors from 'cors';
+import express, {Request} from 'express'
 const PORT = process.env.PORT || 3000;
 
 const app = express()
@@ -11,6 +8,12 @@ const app = express()
 app.use(express.json());
 
 app.use("/", routes);
+app.use(cors<Request>());
+// app.use(cors({
+//     origin: 'http://localhost:5173', // change to your Vue dev server address
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Authorization']
+//   }));
 
 app.listen(PORT, () => {
     console.log(`Server started at http://localhost:${PORT}`);
