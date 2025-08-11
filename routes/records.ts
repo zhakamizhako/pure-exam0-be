bd68b1f356d41eff6828dcab1b6f1ce6ce0866cb
Roland Kim Andre Solon <solon.rolandkimandre@gmail.com>
2025-08-11 17:53:21 +0800

    Rotues

--- routes/records.ts
+++ routes/records.ts
@@ -1,10 +1,20 @@
 import { Router } from "express";
-import { deleteRecord, fetchRecords, insertRecord, updateRecord } from "../controllers/recordController";
+import { deleteRecord, fetchRecords, findRecord, insertRecord, updateRecord } from "../controllers/recordController";
+import { findAgent, fetchAgents, insertAgent, deleteAgent, updateAgent } from "../controllers/agentController";
 
 const router = Router();
 
+// property
 router.route("/records").get(fetchRecords);
 router.route("/records/:id").put(updateRecord);
+router.route("/records/:id").put(findRecord);
 router.route("/records/:id").delete(deleteRecord)
 router.route("/records").post(insertRecord)
+
+//agents
+router.route("/agents").get(fetchAgents);
+router.route("/agents/:id").get(findAgent);
+router.route("/agents/:id").put(updateAgent);
+router.route("/agents/:id").delete(deleteAgent)
+router.route("/agents").post(insertAgent)
 export default router;
